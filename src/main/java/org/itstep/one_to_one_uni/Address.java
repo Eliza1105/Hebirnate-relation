4fe256fc6769cdb6c9b092993bde4b9c10d50f14
gk <lelizaveta776@gmail.com>
2024-08-05 21:13:59 +0300

    hi

---
+++ src/main/java/org/itstep/one_to_one_uni/Address.java
@@ -0,0 +1,19 @@
+package org.itstep.one_to_one_uni;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="address")
+public class Address {
+    @Id
+    @Column(name="id")
+
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;
+    private String street;
+}
